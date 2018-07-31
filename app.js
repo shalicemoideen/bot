f1eda4f2bcf684463ea27c3c27f5217e459bacb2
Shalice Moideen <shalice.moideen@marlabs.com>
2018-07-31 14:25:59 +0530

    ssl certificate

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
 // This loads the environment variables from the .env file
 // require('dotenv-extended').load();
+const https = require("https"),
+fs = require("fs");
 
 const builder = require('botbuilder');
 const restify = require('restify');
 
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+var https_options = {
+  key: fs.readFileSync('/home/shalice/certs/root_ca.key'),
+  certificate: fs.readFileSync('/home/shalice/certs/root_ca.pem')
+};
+
+// var server = restify.createServer();
+var https_server = restify.createServer(https_options);
+console.log(https_server);
+
+// const server = restify.createServer();
+https_server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', https_server.name, https_server.url);
 });
+
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//     console.log('%s listening to %s', server.name, server.url);
+// });
+
+
 
 const connector = new builder.ChatConnector({
     appId: '8feb10fb-5cf3-4adc-b681-0f5aac9ae13a',
     appPassword: 'naflDEI550#+vjfQGJN95=@'
 });
 
-server.post('/api/messages', connector.listen());
+https_server.post('/api/messages', connector.listen());
 
 // Bot Storage: Here we register the state storage for your bot. 
 // Default store: volatile in-memory store - Only for prototyping!
